9f55f5b8aa64c077c9c447134f15ae33fdf3cdef
SANTIAGO PEREZ <perezperret@gmail.com>
2016-11-13 10:15:37 -0400

    v1 escape_chars

---
+++ escape_chars/escape_chars.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void escape_chars(char target[], char source[]);
+
+int main()
+{
+  char * source = "An escaped\t\tweird little\n\tstring";
+  char target[100];
+
+  escape_chars(target, source);
+  printf("source: %s\n", source);
+  printf("target: %s\n", target);
+
+  return 0;
+}
+
+void escape_chars(char target[], char source[])
+{
+  int i = 0; // Index to write to in target
+  int j = 0; // Index to read from in source
+
+  while (source[j] != '\0') {
+    switch (source[j]) {
+    case '\n':
+      target[i] = '/';
+      target[++i] = 'n';
+      break;
+    case '\t':
+      target[i] = '/';
+      target[++i] = 't';
+      break;
+    default:
+      target[i] = source[j];
+      break;
+    }
+
+    ++i;
+    ++j;
+  }
+
+  target[i] = '\0';
+}
