6824db21ebcefe4899468cfe7a187bd095ce86ef
SANTIAGO PEREZ <perezperret@gmail.com>
2016-11-13 10:31:24 -0400

    v1 apply_chars

---
+++ apply_chars/apply_chars.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void apply_chars(char target[], char source[]);
+
+int main()
+{
+  char * source = "An escaped\\t\\tweird little\\n\\tstring";
+  char target[100];
+
+  apply_chars(target, source);
+  printf("source: %s\n", source);
+  printf("target: %s\n", target);
+
+  return 0;
+}
+
+void apply_chars(char target[], char source[])
+{
+  int i = 0; // Index to write to in target
+  int j = 0; // Index to read from in source
+
+  while (source[j] != '\0') {
+
+    if (source[j] == '\\') {
+      switch (source[++j]) {
+      case 'n':
+        target[i] = '\n';
+        break;
+      case 't':
+        target[i] = '\t';
+        break;
+      default:
+        target[i] = source[--j];
+        break;
+      }
+    } else {
+      target[i] = source[j];
+    }
+
+    ++i;
+    ++j;
+  }
+
+  target[i] = '\0';
+}
