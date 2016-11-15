dc3868725904717215820d5b5106be5b2bd7c700
SANTIAGO PEREZ <perezperret@gmail.com>
2016-11-14 22:23:50 -0400

    v1 strindex

--- pseudo_grep/functions.c
+++ pseudo_grep/functions.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAXLINE 1000
 
 int get_line(char vector[], int max_line);
+int strindex(char substr[], char supstr[], int n);
 
 int main()
 {
@@ -11,7 +13,11 @@ int main()
     printf("%s-", vector);
   }
   printf("\n");
-  return 0;
+
+  char * supstr = "I once had very good friend with only one eye in his face"; // length = 56
+  char * substr = "one"; // index = 38
+  int len = 56;
+  printf("%d\n", strindex(substr, supstr, len));
 }
 
 int get_line(char vector[], int max_line)
@@ -24,13 +30,34 @@ int get_line(char vector[], int max_line)
     if (c == EOF) {
       return 0;
     }
+    // Copy valid char
     vector[i] = c;
     ++ i;
   }
+  // Close string
   vector[i] = '\0';
+  // Return string length
   return i;
 }
 
-int strindex(char string[], int n) {
-  return 0;
+int strindex(char substr[], char supstr[], int n) {
+  int i, j;
+  int index = -1;
+  bool found = 0;
+  for (i = 0; i < n; i++) {
+    if (supstr[i] == substr[0]) {
+      found = 1;
+      j = 0;
+      while (substr[j] != '\0') {
+        if (substr[j] != supstr[i + j]) {
+          found = 0;
+          break;
+        }
+        ++ j;
+      }
+      if (found)
+        return i;
+    }
+  }
+  return index;
 }
