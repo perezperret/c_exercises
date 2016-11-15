d2da4455d6cb9d07211ba3ed1d83bb9a6d0d5f86
SANTIAGO PEREZ <perezperret@gmail.com>
2016-11-14 23:18:48 -0400

    V2 pseudo_grep docs

--- pseudo_grep/main.c
+++ pseudo_grep/main.c
@@ -2,6 +2,13 @@
 
 #define MAXLINE 1000
 
+/**
+ * Example from chapter 4 K&R
+ * V1 self made
+ * V2 improved with book
+ * Specific case of UNIX `grep` (only substrings)
+ */
+
 int get_line(char vector[], int max_line);
 int strindex(char source[], char pattern[]);
 
@@ -11,7 +18,9 @@ int main()
   char pattern[] = "ould";
   int found = 0;
 
+  // Iterate through lines
   while (get_line(source, MAXLINE))
+    // If pattern is found, print it
     if (strindex(source, pattern) >= 0) {
       printf("%s", source);
       ++ found;
