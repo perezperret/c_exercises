d4cd2a781cb78967533ff6886c9ed04b4a7b6bc4
SANTIAGO PEREZ <perezperret@gmail.com>
2016-11-23 20:49:37 -0400

    v1 itoa (recursive)

--- itoa/itoa.c
+++ itoa/itoa.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #define MAXLEN 100
 
+/* Two implementations of simple int to char array conversion */
+
 void itoa_loop(int input, char output[], int length);
-void itoa_recursive(int input, char output[], int length);
+void itoa_recursive(int input);
 void reverse(char input[], int length);
 
 int main()
@@ -12,8 +14,23 @@ int main()
   int i = 0;
 
   itoa_loop(input, output, MAXLEN);
-
   printf("%s\n", output);
+
+  itoa_recursive(input);
+  putchar('\n');
+}
+
+void itoa_recursive(int c)
+{
+  if (c < 0) {
+    putchar('-');
+    c = -c;
+  }
+
+  if (c / 10) {
+    itoa_recursive(c / 10);
+  }
+  putchar((c % 10) + '0');
 }
 
 void itoa_loop(int c, char o[], int n)
@@ -28,7 +45,8 @@ void itoa_loop(int c, char o[], int n)
   reverse(o, i);
 }
 
-void reverse(char o[], int n) {
+void reverse(char o[], int n)
+{
   int i = 0;
   int j = n - 1;
   int t;
